a0c16ff98a278724e4733aea79441601635ae753
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-19 15:00:29 +0300

    call pro..

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-let success = true;
+// let success = true;
 
-function getUser(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve([
-                { username: 'john', email: 'john@test.com' },
-                { username: 'jane', email: 'jane@test.com' },
-            ]);
-        }, 1000);
-    });
+// function getUser(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve([
+//                 { username: 'john', email: 'john@test.com' },
+//                 { username: 'jane', email: 'jane@test.com' },
+//             ]);
+//         }, 1000);
+//     });
+// }
+
+// function onFulfilled(users){
+//     console.log(users)
+// }
+// function onRejected(error){
+//     console.log(error);
+// }
+
+// const promise = getUser();
+// promise.then(onFulfilled, onRejected)
+
+
+// The following example changes the success flag to false to simulate the error scenario:
+
+let success = false;
+
+function getUsers() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve([
+          { username: 'john', email: 'john@test.com' },
+          { username: 'jane', email: 'jane@test.com' },
+        ]);
+      } else {
+        reject('Failed to the user list');
+      }
+    }, 1000);
+  });
 }
 
-function onFulfilled(users){
-    console.log(users)
-}
-function onRejected(error){
-    console.log(error);
-}
+const promise = getUsers();
 
-const promise = getUser();
-promise.then(onFulfilled, onRejected)
+promise.catch((error) => {
+  console.log(error);
+});
